43e484b6600fca2cbb0e4ccebab1ee47d8d3cffb
田口　勝貴 <d0811tgc@gmail.com>
2025-08-06 21:01:07 +0900

    Userモデル:リレーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :articles, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
