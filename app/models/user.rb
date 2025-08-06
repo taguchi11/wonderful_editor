61554737b48182c777cc2659426b599fe0b44a85
田口　勝貴 <d0811tgc@gmail.com>
2025-08-06 21:04:12 +0900

    Revert "Userモデル:リレーション設定"
    This reverts commit 43e484b6600fca2cbb0e4ccebab1ee47d8d3cffb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  has_many :articles, dependent: :destroy
-  has_many :article_likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
 end
