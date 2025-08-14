77daaaf697ec06927b2a895cedaa1faa4772809a
田口　勝貴 <d0811tgc@gmail.com>
2025-08-14 15:41:48 +0900

    Articleモデルvalidates設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   has_many   :comments, dependent: :destroy
   belongs_to :user, dependent: :destroy
+  validates :titile, presence: true
+  validates :body, presence: true
 end
