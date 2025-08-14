405f8a2ba85e8462f0849ed5814839d04b8f4bf8
田口　勝貴 <d0811tgc@gmail.com>
2025-08-15 02:39:54 +0900

    Commentモデルvalidates設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   has_many   :comments, dependent: :destroy
   belongs_to :user, dependent: :destroy
-  validates :titile, presence: true
+  validates :title, presence: true
   validates :body, presence: true
   validates :title, length: { maximum: 20 }
   validates :body, length: { maximum: 166 }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  validates :body, length: { maximum: 166 }
+  validates :comment, presence: true
 end
