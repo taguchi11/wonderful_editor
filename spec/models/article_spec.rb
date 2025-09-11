00a211788f56add6fcfa45282a4f152fc8ea2e48
田口　勝貴 <d0811tgc@gmail.com>
2025-09-11 23:29:28 +0900

    Rspec:Articleモデル用2

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,22 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  describe 'articleモデルのテスト' do
+  describe "articleモデルのテスト" do
 	  context "articleが登録できること" do
 	    it "有効なarticleの場合は保存されるか" do
 	      expect(build(:article)).to be_valid
 	    end
 	  end
-
     context "空白のバリデーションチェック" do
       it "titleが空白の場合にエラーメッセージが返ってくるか" do
         article = build(:article, title: nil)
         article.valid?
         expect(article.errors[:title]).to include("を入力してください")
       end
-
     end
-
     context "文字数のバリデーションチェック" do
       it "titleの文字数が21文字以上の場合エラーメッセージが返ってくるか" do
         article = build(:article, title: "あいうえおあいうえおあいうえおあいうえおあ")
