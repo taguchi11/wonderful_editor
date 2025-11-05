9033aa59fb00d6d64e4153767baae43f15ef3bb5
田口　勝貴 <d0811tgc@gmail.com>
2025-11-06 00:43:30 +0900

    API階層追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # devise_for :users
   # mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :v1 do
-    resources :articles, only: [:index, :show]
+  namespace :api do
+    namespace :v1 do
+      resources :articles, only: [:index, :show]
+    end
   end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 RSpec.describe "Articles", type: :request do
   describe "記事の一覧を取得できた場合" do
     # binding.pry
     it "ステータスコード：200を返す" do
-      get "/v1/articles"
+      get "/api/v1/articles"
       binding.pry
       expect(response).to have_http_status(200)
       binding.pry
