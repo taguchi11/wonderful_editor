80ccb328e80d537bcbefaca555a697b0a042df05
田口　勝貴 <d0811tgc@gmail.com>
2025-11-09 18:39:42 +0900

    記事一覧取得処理作成(index)、API::V1構成作成

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class ArticlesController < BaseApiController
+      def index
+        articles = Article.all
+        render json: articles, status: :ok
+      end
+
+  #   def show
+  #     article = Article.find(params[:id])
+  #     render json: article
+  #   end
+
+  #   def create
+  #     article = Article.create!(article_params)
+  #     render json: article
+  #   end
+
+  #   private
+
+  #     def article_params
+  #       params.require(:article).permit(:title, :body)
+  #   end
+    end
+  end
+end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,12 +1,9 @@
 require "rails_helper"
 RSpec.describe "Articles", type: :request do
   describe "記事の一覧を取得できた場合" do
-    # binding.pry
     it "ステータスコード：200を返す" do
       get "/api/v1/articles"
-      binding.pry
       expect(response).to have_http_status(200)
-      binding.pry
     end
   end
 end
